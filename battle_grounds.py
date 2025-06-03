be4f6270a465f501ff2be4c35c4ad2a233a43f3b
Agnogh <agnogh_orion@yahoo.com>
2025-06-03 23:33:45 +0200

    Correcting typo in  list of monsters, adding function for displaying characters

--- battle_grounds.py
+++ battle_grounds.py
@@ -2,10 +2,19 @@
 
 """ creating player character"""
 player_character = {
-    1: {"name": "Guard", "hp": 5, "armour": 1, "damage": (0, 5)},
+    1: {"class": "Guard", "hp": 5, "armour": 1, "damage": (0, 5)},
 }
 
 """ creating enemy characters"""
 monster_charcters = {
-    1: {"name": "Vampire", "hp": 6, "armour": 0, "damage": (2, 4)},
+    1: {"class": "Vampire", "hp": 6, "armour": 0, "damage": (2, 4)},
 }
+
+def display_characters():
+    print("\nAvailable warriors of light: ")
+    for key, value in characters.items():
+        shield = value.get('shield', 0)
+        armour = value.get('armour', 0)
+
+
+print(f"{key}: {value['class']} - HP: {value['hp']}, Armor: {value.get('armor', 0)}, Damage: {value['damage']}")
