8a67977d42e8c71b7c4b660d0a622f3e78087753
Agnogh <agnogh_orion@yahoo.com>
2025-08-29 01:03:20 +0200

    Linking spreadsheet to project

--- battle_grounds.py
+++ battle_grounds.py
@@ -1,6 +1,21 @@
-""" this should be low version of turn based gaem with elements of RPG"""
-
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('pythonbattlefield-34d11d65bfdc.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('python_battlefield')
+
+print([ws.title for ws in SHEET.worksheets()])
+
+""" this should be low version of turn based gaem with elements of RPG"""
 
 """ creating player character"""
 
