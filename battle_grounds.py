619a01bffb1abf6fb2f8cedf79beb006ee188dfc
Agnogh <agnogh_orion@yahoo.com>
2025-08-30 17:37:00 +0200

    Remove general Drive scope as based on the forums and internet, it is not needed and it is safer in case someone snatches my account

--- battle_grounds.py
+++ battle_grounds.py
@@ -4,8 +4,7 @@ from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive.file"
     ]
 
 CREDS = Credentials.from_service_account_file(
