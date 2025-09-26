3ced4387746fbf764adbed5c786499e1fa458fc2
Agnogh <agnogh_orion@yahoo.com>
2025-09-26 20:18:47 +0200

    Remove service account JSON from history ignore (it was not pushed luckily, but commit contained it so had to reset it, then restore it)

--- battle_grounds.py
+++ battle_grounds.py
@@ -21,7 +21,7 @@
 # adding just random comment so I save it so I can commit it
 # so i have the version before merging template from
 # https://github.com/Code-Institute-Org/python-essentials-template.git
-# as this si the one I need to use in order to have my project 
+# as this si the one I need to use in order to have my project
 # accepted. Maybe that is why I had issues with Heroku
 
 import gspread
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# this should call my game entry point - run.py
+from battle_grounds import main  # <-- your file's main()
+
+if __name__ == "__main__":
+    main()
